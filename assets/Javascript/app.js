411bdcacd6d9bce769b3deee26af249c6d8f7bd8
Drew Pepin <drewpepin1995@gmail.com>
2019-07-27 16:17:45 -0400

    Creating question/answer variables

---
+++ assets/Javascript/app.js
@@ -0,0 +1,44 @@
+var questions = [
+    {
+        prompt: "How many NBA championships did Michael Jordan win with the Chicago Bulls? \n(a) Five\n(b) Six\n(c) Seven\n(d) Four",
+        answer: "b"
+    },
+    {
+        prompt: "Who is the all-time leading scorer (total points) in NHL history? \n(a) Sidney Crosby\n(b) Bobby Orr\n(c) Wayne Gretzky\n(d) Mark Messier",
+        answer: "c"
+    },
+    {
+        prompt: "Which American Football team won the first two Super Bowls? \n(a) New England Patriots\n(b) Dallas Cowboys\n(c) Green Bay Packers\n(d) Washington Redskings",
+        answer: "c"
+    },
+    {
+        prompt: "Who holds the record for most no-hitters thrown in MLB history? \n(a) Nolan Ryan\n(b) Pedro Martinez\n(c) Roger Clemens\n(d) Dallas Braden",
+        answer: "a"
+    },
+    {
+        prompt: "Who holds the record for the most touchdowns thrown in a single NFL season? \n(a) Peyton Manning\n(b) Tom Brady\n(c) Patrick Mahomes\n(d) Dan Marino",
+        answer: "a"
+    },
+    {
+        prompt: "Which NHL team is located in Illinois? \n(a) the Bruins\n(b) the Golden Knights\n(c) the Stars\n(d) the Blackhawks",
+        answer: "d"
+    },
+    {
+        prompt: "When was the 3 point line first introduced in an NBA game? \n(a) 1979\n(b) 1951\n(c) 1966\n(d) 2001",
+        answer: "a"
+    },
+    {
+        prompt: "Who has the most homeruns in MLB history for a non-American born player? \n(a) Vlad Guerrero\n(b) Manny Ramirez\n(c) David Ortiz\n(d) Albert Pujols",
+        answer: "d"
+    },
+    {
+        prompt: "Who is the shortest player in NBA history? \n(a) Steve Nash\n(b) Muggsy Bogues\n(c) Spud Webb\n(d) Isaiah Thomas",
+        answer: "b"
+    },
+    {
+        prompt: "How many NHL teams are located in the state of New York? \n(a) 1\n(b) 2\n(c) 3\n(d) 4",
+        answer: "c"
+    }
+] 
+var correctAnswers = 0;
+var incorrectAnswers = 0;
