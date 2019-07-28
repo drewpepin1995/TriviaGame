142cf9a09200230139c7404a4ab155ebea859509
Drew Pepin <drewpepin1995@gmail.com>
2019-07-28 19:14:09 -0400

    Adding questions/answers to page onClick

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,44 +1,35 @@
-var questions = [
-    {
-        prompt: "How many NBA championships did Michael Jordan win with the Chicago Bulls? \n(a) Five\n(b) Six\n(c) Seven\n(d) Four",
-        answer: "b"
-    },
-    {
-        prompt: "Who is the all-time leading scorer (total points) in NHL history? \n(a) Sidney Crosby\n(b) Bobby Orr\n(c) Wayne Gretzky\n(d) Mark Messier",
-        answer: "c"
-    },
-    {
-        prompt: "Which American Football team won the first two Super Bowls? \n(a) New England Patriots\n(b) Dallas Cowboys\n(c) Green Bay Packers\n(d) Washington Redskings",
-        answer: "c"
-    },
-    {
-        prompt: "Who holds the record for most no-hitters thrown in MLB history? \n(a) Nolan Ryan\n(b) Pedro Martinez\n(c) Roger Clemens\n(d) Dallas Braden",
-        answer: "a"
-    },
-    {
-        prompt: "Who holds the record for the most touchdowns thrown in a single NFL season? \n(a) Peyton Manning\n(b) Tom Brady\n(c) Patrick Mahomes\n(d) Dan Marino",
-        answer: "a"
-    },
-    {
-        prompt: "Which NHL team is located in Illinois? \n(a) the Bruins\n(b) the Golden Knights\n(c) the Stars\n(d) the Blackhawks",
-        answer: "d"
-    },
-    {
-        prompt: "When was the 3 point line first introduced in an NBA game? \n(a) 1979\n(b) 1951\n(c) 1966\n(d) 2001",
-        answer: "a"
-    },
-    {
-        prompt: "Who has the most homeruns in MLB history for a non-American born player? \n(a) Vlad Guerrero\n(b) Manny Ramirez\n(c) David Ortiz\n(d) Albert Pujols",
-        answer: "d"
-    },
-    {
-        prompt: "Who is the shortest player in NBA history? \n(a) Steve Nash\n(b) Muggsy Bogues\n(c) Spud Webb\n(d) Isaiah Thomas",
-        answer: "b"
-    },
-    {
-        prompt: "How many NHL teams are located in the state of New York? \n(a) 1\n(b) 2\n(c) 3\n(d) 4",
-        answer: "c"
-    }
-] 
-var correctAnswers = 0;
-var incorrectAnswers = 0;
+let correctAnswers = 0;
+let incorrectAnswers = 0;
+const questionOne = "1. How many NBA championships did Michael Jordan win with the Chicago Bulls?"
+const questionTwo = "2. Who is the all-time leading scorer (total points) in NHL history?"
+const questionThree = "3. Which American Football team won the first two Super Bowls?"
+const questionFour = "4. Who holds the record for most no-hitters thrown in MLB history?"
+const questionFive = "5. Who holds the record for the most touchdowns thrown in a single NFL season?"
+const questionSix = "6. Which NHL team is located in Illinois?"
+const questionSeven = "7. When was the 3 point line first introduced in an NBA game?"
+const questionEight = "8. Who has the most homeruns in MLB history for a non-American born player?"
+const questionNine = "9. Who is the shortest player in NBA history?"
+const questionTen = "10. How many NHL teams are located in the state of New York?"
+
+$(document).ready(function(){
+    $("#startButton").on("click", function(){
+        $("#startButton").toggle();
+        $("#quizForm").toggle();
+        $("#questionOne").text(questionOne);
+        $("#questionTwo").text(questionTwo);
+        $("#questionThree").text(questionThree);
+        $("#questionFour").text(questionFour);
+        $("#questionFive").text(questionFive);
+        $("#questionSix").text(questionSix);
+        $("#questionSeven").text(questionSeven);
+        $("#questionEight").text(questionEight);
+        $("#questionNine").text(questionNine);
+        $("#questionTen").text(questionTen);
+
+
+        
+    });
+
+
+});
+
